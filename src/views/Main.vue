c2e332a5a5800e845b526597dbb7b30ec5ac5ad9
Simon <simimueller@hotmail.com>
2020-11-13 10:17:42 +0100

    move components to new main page

---
+++ src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <FlyTo />
+</template>
+
+<script>
+// @ is an alias to /src
+import FlyTo from "../components/FlyTo.vue";
+
+export default {
+    name: "Main",
+    components: {
+        FlyTo,
+    },
+};
+</script>
+
+<style scoped>
+body {
+    margin: 0;
+    padding: 0;
+}
+</style>
