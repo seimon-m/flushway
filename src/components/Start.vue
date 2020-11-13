006f733aef0dc8de218b95a1f8b5d85759345fc4
Simon <simimueller@hotmail.com>
2020-11-13 11:12:16 +0100

    add start page with input field

---
+++ src/components/Start.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="start">
+        <h1>WOHIN FLIESST DEIN ABWASSER, WENN DU SPÜHLST?</h1>
+        <form class="demo" role="adress">
+            <label for="search" class="hidden-visually">Adresse: </label>
+            <input
+                type="text"
+                name="adress"
+                id="adress"
+                @input="handleChange"
+            />
+            <button type="button" @click="$emit('set-start')">
+                Flush
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Start",
+    props: ["value"],
+    methods: {
+        handleChange(e) {
+            this.$emit("handle-change", e.target.value);
+        },
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hidden-visually {
+    position: absolute;
+    overflow: hidden;
+    white-space: nowrap;
+    margin: 0;
+    padding: 0;
+    height: 1px;
+    width: 1px;
+    clip: rect(0 0 0 0);
+    clip-path: inset(100%);
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,33 @@
 <template>
-    <FlyTo />
+    <Start
+        v-if="!start"
+        @set-start="start = true"
+        @handle-change="handleChange($event)"
+    />
+    <FlyTo v-if="start" :adress="adress" />
 </template>
 
 <script>
 // @ is an alias to /src
 import FlyTo from "../components/FlyTo.vue";
+import Start from "../components/Start.vue";
 
 export default {
     name: "Main",
+    data() {
+        return {
+            start: false,
+            adress: "",
+        };
+    },
     components: {
         FlyTo,
+        Start,
+    },
+    methods: {
+        handleChange(payload) {
+            this.adress = payload;
+        },
     },
 };
 </script>
